ca5101b36764b78c0089d272d6dcdc80f61ada34
Terraknighty <johnjosephpableo@Gmail.com>
2024-04-25 19:57:44 +0800

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ fractal_dimension_w = st.number_input('Enter the worst fractal dimension',min_va
 
 input = np.array([radius_m,texture_m,perimeter_m,area_m,smoothness_m,compactness_m,concavity_m,concave_points_m,symmetry_m,fractal_dimension_m,
                  radius_se,texture_se,perimeter_se,area_se,smoothness_se,compactness_se,concavity_se,concave_points_se,symmetry_se,fractal_dimension_se,
-                 radius_w,texture_w,perimeter_w,area_w,smoothness_w,compactness_w,concavity_w,concave_points_w,symmetry_w,fractal_dimension_w], dtype=np.float64) 
+                 radius_w,texture_w,perimeter_w,area_w,smoothness_w,compactness_w,concavity_w,concave_points_w,symmetry_w,fractal_dimension_w]) 
 
 
 if st.button('Classify'):
